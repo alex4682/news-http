35ede2b03f77ccff502a1ec371e17c22aad3a97b
alex4682 <smoliarenko.sasha@gmail.com>
2025-09-21 13:04:50 +0300

    made task

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+  state = {
+    api: this.props.api,
+    news: null,
+    page: this.props.page
+  }
+
+  async getData() {
+    try {
+      const response = await fetch(this.state.api);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      return null;
+    }
+  }
+
+  getNews = async () => {
+    const data = await this.getData();
+    if (data) {
+      this.setState({ news: data.hits });
+      console.log(data.hits);
+    }
+  }
+
+  componentDidMount() {
+    this.getNews();
+  }
+
+  prevPage = () => {
+    if (this.state.page > 0) {
+      const newPage = this.state.page - 1;
+      const newApi = `http://hn.algolia.com/api/v1/search_by_date?tags=story&page=${newPage}&hitsPerPage=21`;
+      this.setState({ page: newPage, api: newApi }, this.getNews);
+    }
+  }
+  nextPage = () => {
+    const newPage = this.state.page + 1;
+    const newApi = `http://hn.algolia.com/api/v1/search_by_date?tags=story&page=${newPage}&hitsPerPage=21`;
+    this.setState({ page: newPage, api: newApi }, this.getNews);
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='btnWrap'><button onClick={this.prevPage}>Prev</button>
+          <button onClick={this.nextPage}>Next</button></div>
+        {this.state.news && (
+          <ul>
+            {this.state.news.map(item => (
+              <li key={item.objectID}>
+                <h2>{item.title}</h2>
+                <p>Author: {item.author}</p>
+                {item.url ? <a href={item.url} target="_blank" rel="noopener noreferrer">Read more</a> : ""}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    );
+  }
+}
